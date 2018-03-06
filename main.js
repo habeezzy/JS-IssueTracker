5e26022b22a394a30ecbd9f1ed05ac9fd7b484ef
habeezzy <mhabeebulah@gmail.com>
2018-03-05 23:49:28 -0500

    second commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-document.getElementById('issueInputForm').addEventListener('submit', saveIssue);  //handles submit event from form//
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
 function saveIssue(e) {
   var issueDesc = document.getElementById('issueDescInput').value;
-  var issueSeverity = document.getElementById("issueSeverityInput").value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
   var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-  var issueId = chance.guid();  //chancejs library returns a global unit identifier
+  var issueId = chance.guid();
   var issueStatus = 'Open';
 
   var issue = {
@@ -15,7 +15,7 @@ function saveIssue(e) {
     status: issueStatus
   }
 
-  if(localStorage.getItem('issues') == null) {
+  if (localStorage.getItem('issues') == null) {
     var issues = [];
     issues.push(issue);
     localStorage.setItem('issues', JSON.stringify(issues));
@@ -25,34 +25,62 @@ function saveIssue(e) {
     localStorage.setItem('issues', JSON.stringify(issues));
   }
 
-  document.getElementById('issueInputForm').reset();  //reset input elements
+  document.getElementById('issueInputForm').reset();
 
   fetchIssues();
 
-  e.preventDefault(); //prevent form from submitting
+  e.preventDefault();
+}
+
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = 'Closed';
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
 }
 
 function fetchIssues() {
-  var issues = JSON.parse(localStorage.getItem('issues'));  //parsing return value from local storage//
-  var issuesList = document.getElementById('issuesList'); //reference to the issues list div element//
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  var issuesListe = document.getElementById('issuesList');
 
-  issuesList.innerHTML = '';  //generating the dynamic list output//
+  issuesList.innerHTML = '';
 
-  for(var i=0;i<issues.length; i++) {
+  for (var i = 0; i < issues.length; i++) {
     var id = issues[i].id;
     var desc = issues[i].description;
     var severity = issues[i].severity;
     var assignedTo = issues[i].assignedTo;
     var status = issues[i].status;
 
-    issuesList.innerHTML += '<div class="well">' + '</div>'
-                            '<h6> Issue ID:' + id + '</h6>' +
-                            '<p><span class="label label-info">' + status + '</span></p>' +
-                            '<h3>' + desc + '</h3>' +
-                            '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
-                            '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
-                            '<a href="#" onclick="setStatusClosed(\' '+ id +'\')" class="btn btn-warning">Close</a>' +
-                            '<a href="#" onclick="deleteIssue(\' '+ id +'\')" class="btn btn-danger">Delete</a>' +
-                            '</div>';
+    issuesList.innerHTML +=   '<div class="well">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
+                              '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                              '</div>';
   }
 }
